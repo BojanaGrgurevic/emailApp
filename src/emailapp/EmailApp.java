11b08a4005461d7085cc6b19492d5f1838863306
BojanaGrgurevic <47015254+BojanaGrgurevic@users.noreply.github.com>
2019-11-26 01:36:52 +0100

    Add files via upload

---
+++ src/emailapp/EmailApp.java
@@ -0,0 +1,12 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1=new Email("Bojana", "Grgurevic");
+	//	em1.setAlternateEmail("bg@gmail.com");
+		System.out.println(em1.showInfo());
+		
+	}
+
+}
